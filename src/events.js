28c34846da198d0a7d12bab4622f1a1490ee00a8
RobinQu <robinqu@gmail.com>
2014-06-04 18:45:57 +0800

    event wrapper

--- src/events.js
+++ src/events.js
@@ -1,7 +1,45 @@
 define(["./context", "./data"], function(boost, $data) {
-  var Events = {}, addEvent, removeEvent, normalizeEvent, NO_BUBBLES;
+  var E, addEvent, removeEvent, normalizeEvent, NO_BUBBLES;
   
   NO_BUBBLES = ["focus", "change", "submit"];
+  
+  E = function(originalEvent) {
+    var i, len, k;
+    if(originalEvent) {//copy attributes
+      for(i=0,len=Event._props; i<len; i++) {
+        k = Event._props[i];
+        this[k] = originalEvent[k];
+      }
+    }
+    
+    var doc, body;
+    this.timeStamp = this.timeStamp || Date.now();
+    if(!this.target) {
+      this.target = this.srcElement || document;
+    }
+    if(this.target.nodeType === 3) {
+      this.target = this.target.parentNode;
+    }
+    if(!this.relatedTarget && this.fromElement) {
+      this.relatedTarget = this.fromElement === this.target ? this.toElement : this.fromElement;
+    }
+    if(this.pageX === undefined && this.clientX !== undefined) {//IE
+      doc = document.documentElement;
+      body = document.body;
+      this.pageX = this.clientX + (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc.clientLeft || 0);
+      this.pageY = this.clientY + (doc && doc.scrollTop || body && body.scrollTop || 0) - (doc.clientTop || 0);
+    }
+    
+    if(!this.which && (originalEvent.charCode === 0 ? this.charCode : this.keyCode)) {
+      this.which = this.charCode || this.keyCode;
+    }
+    
+    //TOOD: metakey, mousewheel, wheelDelta
+    
+  };
+  
+  
+  E._props = "altKey attrChange attrName bubbles button cancelable charCode clientX clientY ctrlKey currentTarget data detail eventPhase fromElement handler keyCode metaKey newValue originalTarget pageX pageY prevValue relatedNode relatedTarget screenX screenY shiftKey srcElement target timeStamp toElement type view which touches targetTouches changedTouches animationName elapsedTime dataTransfer".split(" ");
   
   addEvent = function(elem, type, capture) {
     var listener = $data(elem, "_listener");
@@ -31,182 +69,191 @@ define(["./context", "./data"], function(boost, $data) {
   };
   
   normalizeEvent = function(event) {
-    
+    if(window.event === event) {
+      return new E(event);
+    }
+    return event.normalized ? event : new E(event);
   };
   
-  Events.add = function(eventType, elem, hanlder, capture) {
-    if(elem.length > 0) {
-      elem.forEach(function(el) {
-        Events.on(el, elem[i], handler);
-      });
-      return this;
-    }
-    
-    var events, handlers, method;
-    
-    if(elem.nodeType === 3 || elem.nodeType === 8) { return this; }
-    
-    if(typeof handler === "function") {// a simple function
-      method = handler;
-      handler = {
-        context: null,
-        method: method
-      };
-    } else {
-      method = handler.method;
-    }
-    events = $data(elem, "_events") || $data(elem, "_events", {});
-    handlers = events[eventType];
-    if(!handlers) {
-      handlers = events[eventType] = {};
-    }
-    handlers[context.hashFor(handler.context, handler.method)] = handler;
-    //disable capture by default
-    addEvent(elem, type, method, false);
-    return this;
-  };
   
-  //create a custom events
-  Events.simulate = function(elem, eventType, attrs) {
-    // mock an event object
-    var event = {
-      type: eventType,
-      target: elem,
-      preventDefault: function() {
-        this.cancelled = true;
-      },
-      stopPropagation: function() {
-        this.bubles = false;
-      },
-      timeStamp: Date.now(),
-      cancelled: false,
-      bubbles: NO_BUBBLES.indexOf(eventType) > -1
-    };
+  context.merge(E, {
     
+    add: function(eventType, elem, hanlder, capture) {
+      if(elem.length > 0) {
+        elem.forEach(function(el) {
+          Events.on(el, elem[i], handler);
+        });
+        return this;
+      }
     
-    return event;
-  };
-  
-  Events.remove = function(elem, eventType, handler) {
-    if(elem.length > 0) {
-      elem.forEach(function(el) {
-        Events.remove(el, eventType, handler);
-      });
-      return this;
-    }
-    var handlers, events, k, clean;
-    clean = false;
-    events = $data(elem, "_events");
-    if(eventType) {
+      var events, handlers, method;
+    
+      if(elem.nodeType === 3 || elem.nodeType === 8) { return this; }
+    
+      if(typeof handler === "function") {// a simple function
+        method = handler;
+        handler = {
+          context: null,
+          method: method
+        };
+      } else {
+        method = handler.method;
+      }
+      events = $data(elem, "_events") || $data(elem, "_events", {});
       handlers = events[eventType];
+      if(!handlers) {
+        handlers = events[eventType] = {};
+      }
+      handlers[context.hashFor(handler.context, handler.method)] = handler;
+      //disable capture by default
+      addEvent(elem, type, method, false);
+      return this;
+    },
+    
+    simulate: function(elem, eventType, attrs) {
+      // mock an event object
+      var event = new E({
+        type: eventType,
+        target: elem,
+        preventDefault: function() {
+          this.cancelled = true;
+        },
+        stopPropagation: function() {
+          this.bubles = false;
+        },
+        timeStamp: Date.now(),
+        cancelled: false,
+        bubbles: NO_BUBBLES.indexOf(eventType) > -1,
+        normalized: true
+      });
+    
+      if(attrs) {
+        context.mixin(event, attrs);
+      }
+      return event;
+    },
+    
+    remove: function(elem, eventType, handler) {
+      if(elem.length > 0) {
+        elem.forEach(function(el) {
+          Events.remove(el, eventType, handler);
+        });
+        return this;
+      }
+      var handlers, events, k, clean;
+      clean = false;
+      events = $data(elem, "_events");
+      if(eventType) {
+        handlers = events[eventType];
       
-      if(handler) {//remove specific event registeration
-        // removeEvent(elem, eventType, handler.method || handler);
-        delete handlers[context.hashFor(handler.context, handler.method)];
-        k = null;
-        for(k in handlers) {//check for other handlers
-          break;
-        }
-        if(!key) {
+        if(handler) {//remove specific event registeration
+          // removeEvent(elem, eventType, handler.method || handler);
+          delete handlers[context.hashFor(handler.context, handler.method)];
+          k = null;
+          for(k in handlers) {//check for other handlers
+            break;
+          }
+          if(!key) {
+            clean = true;
+          }
+        } else {//clean all handlers for this event
           clean = true;
         }
-      } else {//clean all handlers for this event
-        clean = true;
+      } else {//remove events for this handler
+        for(k in events) {
+          Events.remove(elem, events[k]);
+        }
       }
-    } else {//remove events for this handler
+      if(clean) {
+        delete events[eventType];
+        removeEvent(elem, eventType);
+      }
+    
+      // check if we need to clean `events`
+      k = null;
       for(k in events) {
-        Events.remove(elem, events[k]);
+        break;
       }
-    }
-    if(clean) {
-      delete events[eventType];
-      removeEvent(elem, eventType);
-    }
+      if(!key) {//no other events found, then cleanup stored data
+        $data(elem, "_events", null);
+        $data(elem, "_listener", null);
+      }
     
-    // check if we need to clean `events`
-    k = null;
-    for(k in events) {
-      break;
-    }
-    if(!key) {//no other events found, then cleanup stored data
-      $data(elem, "_events", null);
-      $data(elem, "_listener", null);
-    }
+      return this;
+    }, 
     
-    return this;
-  };
-  
-  Events.trigger = function(elem, eventType, args) {
-    var fn, event, current, ret;
-    if(elem.length && elem.length > 0) {
-      elem.forEach(function(el) {
-        Events.trigger(el, eventType);
-      });
-      return this;
-    }
-    if(elem.nodeType === 3 || elem.nodeType === 8) {
-      return this;
-    }
-    if(typeof elem[eventType] === "function") {//native event type
-      fn = elem[eventType];
-    }
-    event = args[0];
-    if(!event) {//when trigger a custom event, no acutal event is given
-      event = Events.simulate(elem, eventType);
+    trigger: function(elem, eventType, args) {
+      var fn, event, current, ret;
+      if(elem.length && elem.length > 0) {
+        elem.forEach(function(el) {
+          Events.trigger(el, eventType);
+        });
+        return this;
+      }
+      if(elem.nodeType === 3 || elem.nodeType === 8) {
+        return this;
+      }
+      if(typeof elem[eventType] === "function") {//native event type
+        fn = elem[eventType];
+      }
+      event = args[0];
+      if(!event) {//when trigger a custom event, no acutal event is given
+        event = Events.simulate(elem, eventType);
+        args.unshift(event);
+      }
+      event.type = eventType;
+    
+      //trigger custom events; do bubbling until `document`
+      current = elem;
+      do {
+        ret = Events.handle(current, event, args);
+        current = (current === document) ? null : (current.parentNode || document);
+      } while(!ret && event.bubbles && current);
+      current = null;
+    
+      //it's not a native event, but has onxxx handlers
+      if(!fn && elem["on" + eventType] && elem["on" + eventType].apply(elem, args) === false) {
+        ret = false;
+      }
+    
+      //trigger native events
+      if(fn && ret !== false) {
+        try {
+          fn();
+        } catch(e) {}
+      }
+    
+      return ret;
+    },
+    
+    handle: function(elem, event, args) {
+      var handlers, k, ret, val;
+      // args = Array.prototype.slice.call(arguments, 0);
+      // elem = args.shift();
+      event = normalizeEvent(event || window.event);
+    
+      handlers = ($data(elem, "_events") || {})[event.type];
+      if(!handlers) {
+        return false;
+      }
+    
       args.unshift(event);
-    }
-    event.type = eventType;
-    
-    //trigger custom events; do bubbling until `document`
-    current = elem;
-    do {
-      ret = Events.handle(current, event, args);
-      current = (current === document) ? null : (current.parentNode || document);
-    } while(!ret && event.bubbles && current);
-    current = null;
-    
-    //it's not a native event, but has onxxx handlers
-    if(!fn && elem["on" + eventType] && elem["on" + eventType].apply(elem, args) === false) {
-      ret = false;
-    }
-    
-    //trigger native events
-    if(fn && ret !== false) {
-      try {
-        fn();
-      } catch(e) {}
-    }
-    
-    return ret;
-  };
-  
-  Events.handle = function(elem, event, args) {
-    var handlers, k, ret, val;
-    // args = Array.prototype.slice.call(arguments, 0);
-    // elem = args.shift();
-    event = normalizeEvent(event || window.event);
-    
-    handlers = ($data(elem, "_events") || {})[event.type];
-    if(!handlers) {
-      return false;
-    }
-    
-    args.unshift(event);
-    for(k in handlers) {
-      handler = handlers[k];
-      ret = handler.method.apply(handler.context, args);
-      if(ret === false) {
-        event.preventDefault();
-        event.stopPropagation();
+      for(k in handlers) {
+        handler = handlers[k];
+        ret = handler.method.apply(handler.context, args);
+        if(ret === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        if(val !== false) {
+          val = ret;
+        }
       }
-      if(val !== false) {
-        val = ret;
-      }
+      //return the result of this event dispatch
+      return val;
     }
-    //return the result of this event dispatch
-    return val;
-  };
+    
+  });
   
-  
-  return Events;
+  return E;
 });
