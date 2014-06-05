64df28c716a74d7efb3272c7a78430dd8847f19e
RobinQu <robinqu@gmail.com>
2014-06-05 18:56:30 +0800

    promise started; xhr

--- src/net/xhr.js
+++ src/net/xhr.js
@@ -1,4 +1,4 @@
-define(["../core", "../runtime/base64", "../runtime/json"], function(boost, $base64, $json) {
+define(["../core", "../runtime"], function(boost) {
   
   // XHR Factory
   // replace this function to support customized XHR
@@ -21,6 +21,11 @@ define(["../core", "../runtime/base64", "../runtime/json"], function(boost, $bas
       this.set(options.headers);
     }
     this.url = options.uri || options.url;
+    this.withCredential = options.withCredential || false;
+    this.timeout = options.timeout || 1000 * 8;
+    this.deferred = boost.deffered();
+    this.response = {};
+    this.timer = null;
   };
   
   Request.prototype.set = function (key, value) {
@@ -32,26 +37,64 @@ define(["../core", "../runtime/base64", "../runtime/json"], function(boost, $bas
           this.set(k, v);
         }
       }
-      return;
+      return this;
     }
     this.xhr.setRequestHeader(key, value);
     return this;
   };
   
   Request.prototype.basic = function (user, password) {
-    this.set("Authorization", "Basic " + $base64.encode(user + ":" + password));
+    this.set("Authorization", "Basic " + boost.base64.encode(user + ":" + password));
     return this;
   };
   
   Request.prototype._handleData = function (data) {
     //stringify plain objects to JSON string
     if(typeof data === "object" && boost.classType(data) === "Object") {//plain object
-      data = $json.stringify(data);
+      data = boost.JSON.stringify(data);
     }
     return data;
   };
   
-  Request.prototype.send = function (method, data, callback) {
+  Request.prototype._handlestatechange = function () {
+    var status, response = {},
+        //Copied from jQuery.ajax
+        rheaders = /^(.*?):[ \t]*([^\r\n]*)\r?$/mg;
+    switch(this.xhr.readyState) {
+    case 2:
+      break;
+    case 4:
+      clearTimeout(this.timer);
+      this.xhr.onreadystatechange = boost.noop;
+      try {//try to build necessary data
+        response.status = status = this.xhr.status;
+        response.text = this.xhr.responseText;
+        response.headers = (function() {
+          var str = this.xhr.getAllResponseHeaders() || "",
+              match,
+              ret = {};
+          
+          while(!!(match = rheaders.exec(str))) {
+            ret[match[1].toLowerCase()] = match[2];
+          }
+        })();
+      } catch(e) {
+      }
+      try {//try to parse data
+        response.body = boost.JSON.parse(response.text);
+      } catch(e) {}
+      
+      if(status >= 400 && status < 500) {
+        this.deffered.reject(new Error("client error"), response);
+      } else if(status >= 500) {
+        this.deffered.reject(new Error("server error"), response);
+      }
+      this.deferred.resolve(response);
+      break;
+    }
+  };
+  
+  Request.prototype.send = function (method, data) {
     if(method && ["POST", "GET", "DELETE", "OPTION", "PUT"].indexOf(method.toUpperCase()) > -1) {
       this.method = method;
     }
@@ -61,10 +104,24 @@ define(["../core", "../runtime/base64", "../runtime/json"], function(boost, $bas
     if(data && ["GET", "OPTION"].indexOf(method.toUpperCase()) === 0) {//we can send data
       this.send(this._handleData());
     }
+    this.xhr.onreadstatechange = this._handlestatechange.bind(this);
+    
+    this.timer = setTimeout(function() {
+      this.abort("time out");
+    }.bind(this), this.timeout);
+    
+    return this.deffered.promise;
   };
   
-  Request.prototype.abort = function () {
+  ["post", "put", "get", "delete", "option"].forEach(function(name) {
+    Request.prototype[name] = function(data) {
+      return this.send(name, data);
+    };
+  });
+  
+  Request.prototype.abort = function (reason) {
     this.xhr.abort();
+    this.deffered.reject(new Error(reason));
   };
   
   boost.Request = Request;
