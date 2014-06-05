422e2c72fb5b6ca3d1d289d9c30c917f4a87537a
RobinQu <robinqu@gmail.com>
2014-06-05 14:45:07 +0800

    elem data

--- src/dom/data.js
+++ src/dom/data.js
@@ -1,4 +1,29 @@
 define(["../core"], function(boost) {
-  boost.data = function(elem, key, value) {};
+  var setValue, getValue, 
+      cache = {}, cacheKey = "__boost_cache_key__",
+      uuid = 0;
+  
+  boost.data = function(elem, key, value) {
+    var entryKey, data;
+    if(elem.dataset) {//support data api
+      entryKey = elem.dataset[cacheKey];
+    } else {
+      entryKey = elem[cacheKey];
+    }
+    if(entryKey) {
+      data = cache[entryKey];
+    } else {
+      elem.dataset[cacheKey] = uuid++;
+      data = cache[uuid] = {};
+    }
+    
+    if(value || value === null) {//setter
+      data[key] = value;
+      return value;
+    }
+    //getter
+    return data[key];
+  };
+  
   return boost.data;
 });
