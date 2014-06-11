33afffbcd5fcbfd19baf95f704d483ba7a2b09e5
RobinQu <robinqu@gmail.com>
2014-06-11 10:25:21 +0800

    mixin support

--- src/runtime/object.js
+++ src/runtime/object.js
@@ -1,16 +1,40 @@
 define(["core"], function(boost) {
   
   var fnTest = /xyz/.test(function(){var xyz;}) ? /\b_super\b/ : /.*/,
-      init;
+      init, XObject;
   
   init = function() {
     if(this.init) {
       this.init.apply(this, arguments);
     }
+    if(this.__mixins__ && this.__mixins__.length) {
+      this.__mixins__.forEach(function(mixin) {
+        if(mixin.initMixin) {
+          mixin.initMixin.apply(this);
+        }
+        boost.merge(this, mixin);
+        // TODO: should avoid merge `initMixin` in the first place
+        delete this.initMixin;
+      }, this);
+    }
   };
   
-  var XObject = function() {
+  XObject = function() {
     init.apply(this, arguments);
+  };
+  
+  XObject.mixin = function mixin() {
+    var proto = this.prototype,
+        mixins = boost.slice(arguments),
+        mx;
+    
+    mx = proto.__mixins__ = (proto.__mixins__ || []);
+    mixins.forEach(function(mixin) {
+      if(mx.indexOf(mixin) === -1) {
+        mx.push(mixin);
+      }
+    });
+    return this;
   };
   
   XObject.extend = function extend(prop) {
@@ -26,10 +50,11 @@ define(["core"], function(boost) {
         this._super = _super[name];
         ret = fn.apply(this, arguments);
         this._super = tmp;
-        
         return ret;
       };
     };
+    
+    //make `_super`
     for(key in prop) {
       if(typeof prop[name] === "function" && typeof  _super[name] === "function" && fnTest.test(prop[name]) ) {
         proto[key] = makeSuper(name, prop[name]);
@@ -37,14 +62,14 @@ define(["core"], function(boost) {
         proto[key] = prop[name];
       }
     }
+    
     Child = function() {
       init.apply(this, arguments);
     };
-    
     Child.prototype = proto;
     Child.prototype.consturcotr = Child;
     Child.extend = extend;
-    
+    Class.mixin = mixin;
     return Class;
   };
   
