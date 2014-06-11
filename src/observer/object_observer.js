ec270b6bd913f79737c784f929b50fdfd28d9cea
RobinQu <robinqu@gmail.com>
2014-06-11 17:09:23 +0800

    spec for ObjectObserver

--- src/observer/object_observer.js
+++ src/observer/object_observer.js
@@ -1,8 +1,10 @@
 define(["runtime", "./observable"], function(boost, observable) {
   
-  if(!window.WeakMap) {return;}
+  if(!Object.observe) {return;}
   
   var logger = boost.Logger.instrument("object_observer");
+  
+  var ChangeTypes = ["add", "delete", "update", "splice"];
   
   // we store all objects reference in this map
   // `WeakMap` will reduce cycling refs
@@ -21,29 +23,49 @@ define(["runtime", "./observable"], function(boost, observable) {
       this._handler = this.handlerTemplate.bind(this);
       
       this.listeners = [];
-      this.isObserving = false;
       this.filter = filter;
+      this.isObserving = false;
     },
     
     handlerTemplate: function(changes) {
-      logger.log("handler", this.listeners.length);
+      logger.log("handler, listeners %s, changes %s", this.listeners.length, changes.length);
+      var sorted = {};
+      changes.forEach(function(change) {
+        var type = change.type;
+        sorted[type] = sorted[type] || [];
+        sorted[type].push(change);
+      });
       this.listeners.forEach(function(l) {
-        l.call(this, changes);
+        //default to accept all changes
+        var wanted = changes;
+        if(l.filter) {
+          logger.log("apply filters", l.filter);
+          wanted = l.filter.reduce(function(prev, cur, i) {
+            // console.log(cur, sorted[cur], prev);
+            return sorted[cur] ? prev.concat(sorted[cur]) : prev;
+          }, []);
+        }
+        l.call(this, wanted);
       }, this);
     },
     
-    connect: function(fn) {
+    connect: function(fn, filter) {
       if(!fn) {
         return this;
       }
       logger.log("connect");
+      
+      //store filter right on the function
+      fn.filter = filter || this.filter;
       var target = observants.get(this);
       if(!this.isObserving) {
         logger.log("observe", target);
-        Object.observe(target, this._handler, this.filter);
+        Object.observe(target, this._handler);
       }
-      this.listeners.push(fn);
-      logger.log("add listener");
+      if(this.listeners.indexOf(fn) === -1) {
+        this.listeners.push(fn);
+      }
+      logger.log("add listener", this.listeners.length);
       return this;
     },
     
@@ -56,8 +78,9 @@ define(["runtime", "./observable"], function(boost, observable) {
         this.listeners.length = 0;
       }
       if(!this.listeners.length) {//unboserve if we don't have any listeners
+        var obj = observants.get(this);
         logger.log("unobserve", obj);
-        Object.unobserve(observants.get(this));
+        Object.unobserve(obj, this._handler);
         this.isObserving = false;
       }
     },
--- test/observer/obectObserverSpec.js
+++ test/observer/obectObserverSpec.js
@@ -12,12 +12,63 @@ define(["boost"], function(boost) {
         expect(changes[0].type).to.equal("add");
         expect(changes[0].name).to.equal("foo");
         done();
+        ob.disconnect();
       });
       
       o.foo = "bar";
       
     });
     
+    it("should respond to update of a property", function(done) {
+      
+      ob.connect(function(changes) {
+        expect(changes).to.be.ok;
+        expect(changes[0].type).to.equal("update");
+        expect(changes[0].name).to.equal("foo");
+        done();
+        ob.disconnect();
+      });
+      
+      o.foo = "qux";
+    
+    });
+    
+    it("should respond to removeal of a property", function(done) {
+      
+      ob.connect(function(changes) {
+        expect(changes).to.be.ok;
+        expect(changes[0].type).to.equal("delete");
+        expect(changes[0].name).to.equal("foo");
+        done();
+        ob.disconnect();
+      });
+      
+      delete o.foo;
+      
+    });
+    
+    
+    it("should support filter", function(done) {
+      
+      ob.connect(function(changes) {
+        expect(changes.length).to.be.above(1);
+        changes.forEach(function(c) {
+          // console.log(c.type);
+          expect(c.type).not.to.equal("delete");
+          expect(c.name).to.equal("bar");
+        });
+        
+        ob.disconnect();
+        done();
+      }, ["add", "update"]);
+      
+      o.bar = "bar";
+      o.bar = "qux";
+      delete o.bar;
+    });
+    
   });
+  
+
   
 });
